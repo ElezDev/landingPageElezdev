194e91da003ec622dc3b4fdc83eaaaa594f57357
ElezDev <edwinledezma2023@gmail.com>
2024-06-09 17:03:16 -0500

    add changes news

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none ">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br />{" "}
+              Hola Soy <br />{" "}
               <span className="text-accent">Edwin Ledezma</span>
             </h1>
             <p className="max-w-[50] mb-9 text-white/80">
